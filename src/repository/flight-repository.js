2c91f46473937b6fbfdfa62a6db4d3b9e655461e
ADITYARAJPUT2505 <ar9412349716@gmail.com>
2024-08-02 22:53:39 +0530

    helper function is added in utils folder

--- src/repository/flight-repository.js
+++ src/repository/flight-repository.js
@@ -10,5 +10,17 @@ class FlightRepository {
       throw error;
     }
   }
+  async getFlight(flightId){
+    try {
+      const flight = await Flights.findByPk({where:{id:flightId}});
+      return flight;
+    } catch (error) {
+      console.error("Error in repository layer");
+      throw error;
+    }
+  }
+  async getAllFlights(){
+
+  }
 }
 module.exports = FlightRepository;
